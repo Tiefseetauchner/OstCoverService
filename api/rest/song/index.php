339ca8e5c374675ee8f3047be039758753afc479
tauchner <7229@htl.rennweg.at>
2020-12-05 03:37:28 +0100

    - Implemented request of multiple IDs

--- api/rest/song/index.php
+++ api/rest/song/index.php
@@ -11,21 +11,24 @@ $conn = new PDO("mysql:host=$servername;dbname=OstCoverService", $username, $pas
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+$databaseRequest = $conn->prepare("select * from songs");
+$id = explode(",", $request["id"]);
 if (array_key_exists("id", $request)) {
-  $id = $request["id"];
-  $databaseRequest = $conn->prepare("select * from songs where id = '$id'");
+  $in  = str_repeat('?,', count($id) - 1) . '?';
+  $databaseRequest = $conn->prepare("select * from songs where id in($in)");
+  $databaseRequest->execute($id);
+} else {
   $databaseRequest->execute();
-
-  $result = $databaseRequest->fetchAll(PDO::FETCH_ASSOC);
-
-  if (empty($result)) {
-    $result = ["error" => "ID not found"];
-  }
-
-  header('Content-Type: application/json');
-
-  $result = json_encode($result);
-
-  echo $result;
 }
 
+$result = $databaseRequest->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($result)) {
+  $result = ["error" => "ID $id not found"];
+}
+
+header('Content-Type: application/json');
+
+$result = json_encode($result);
+
+echo $result;
