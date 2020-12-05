1a40e487e15014b851e251c34f081559d88f9b33
tauchner <7229@htl.rennweg.at>
2020-12-05 02:33:36 +0100

    - Made test site more readable

--- index.php
+++ index.php
@@ -15,32 +15,38 @@ spl_autoload_register(function ($class) {
 <body>
 <h1>API Test Site</h1>
 <h2>Song Tests</h2>
-<?php
-$testDataGenerator = new SongTestDataGenerator();
-for ($i = 0; $i < 10; $i++) {
-  echo json_encode($testDataGenerator->testData, JSON_PRETTY_PRINT);
-  echo "<br>";
-  $testDataGenerator->generateTestData();
-}
-?>
+<code>
+  <?php
+  $testDataGenerator = new SongTestDataGenerator();
+  for ($i = 0; $i < 10; $i++) {
+    echo json_encode($testDataGenerator->testData, JSON_PRETTY_PRINT);
+    echo "<br>";
+    $testDataGenerator->generateTestData();
+  }
+  ?>
+</code>
 <h2>OST Tests</h2>
-<?php
-$testDataGenerator = new OriginalSoundTrackTestDataGenerator();
-for ($i = 0; $i < 10; $i++) {
-  echo json_encode($testDataGenerator->testData, JSON_PRETTY_PRINT);
-  echo "<br>";
-  $testDataGenerator->generateTestData();
-}
-?>
+<code>
+  <?php
+  $testDataGenerator = new OriginalSoundTrackTestDataGenerator();
+  for ($i = 0; $i < 10; $i++) {
+    echo json_encode($testDataGenerator->testData, JSON_PRETTY_PRINT);
+    echo "<br>";
+    $testDataGenerator->generateTestData();
+  }
+  ?>
+</code>
 <h2>Mandatory (but useless) seeder class test</h2>
 <p>I'm protesting this. It is stupid to make people program extremely ugly code. You want them to learn, how to code,
     not how to make a class that returns static data. The way you phrased your exercise, most people will just write a
     class that returns static data. I think, that is stupid when considering that we should learn, how to write
     expandable and stackable code.</p>
 <p>But here you have your seeder class output:</p>
-<?php
-echo json_encode(new SeederClass(), JSON_PRETTY_PRINT);
-?>
+<code>
+  <?php
+  echo json_encode(new SeederClass(), JSON_PRETTY_PRINT);
+  ?>
+</code>
 </body>
 </html>
 
