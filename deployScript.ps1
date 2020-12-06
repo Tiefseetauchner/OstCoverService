7bc18d40a4e65e29b3cf4e53b0ba1e5538b2f166
tauchner <7229@htl.rennweg.at>
2020-12-06 14:34:29 +0100

    - Reformatted Deploy Script

--- deployScript.ps1
+++ deployScript.ps1
@@ -1,16 +1,17 @@
-if (Test-Path "createTestData.sql") {
-    [system.reflection.assembly]::LoadWithPartialName("MySql.Data")    
+if (Test-Path "createTestData.sql")
+{
+    [system.reflection.assembly]::LoadWithPartialName("MySql.Data")
     $mysql_server = "localhost"
     $mysql_user = "root"
     $mysql_password = "eah+S`;<6Pj=5-m["
     write-host "Create coonection to db1"
     # Connect to MySQL database 'db1'
-    
+
     $cn = New-Object -TypeName MySql.Data.MySqlClient.MySqlConnection
     $cn.ConnectionString = "SERVER=$mysql_server;UID=$mysql_user;PWD=$mysql_password"
     $cn.Open()
     write-host "Running backup script against db1"
-    # Run Update Script MySQL 
+    # Run Update Script MySQL
     $cm = New-Object -TypeName MySql.Data.MySqlClient.MySqlCommand
     $sql = Get-Content "createTestData.sql"
     $cm.Connection = $cn
@@ -18,7 +19,9 @@ if (Test-Path "createTestData.sql") {
     $cm.ExecuteReader()
     write-host "Closing Connection"
     $cn.Close()
-} else {
+}
+else
+{
     Write-Warning "No Database creation script found. Database will not be updated"
 }
 
