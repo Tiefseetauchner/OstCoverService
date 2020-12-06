30c0681233e6ecd2584736c5a4045a2019e5b4ff
tauchner <7229@htl.rennweg.at>
2020-12-07 00:22:18 +0100

    - I'm to dumb to install mysql. My Deploy Script doesn't work. Someone help

--- deployScript.ps1
+++ deployScript.ps1
@@ -1,30 +1,24 @@
 if (Test-Path "createTestData.sql")
 {
-    [system.reflection.assembly]::LoadWithPartialName("MySql.Data")
-    $mysql_server = "localhost"
-    $mysql_user = "root"
-    $mysql_password = "eah+S`;<6Pj=5-m["
-    write-host "Create coonection to db1"
-    # Connect to MySQL database 'db1'
+    try {
+        $mysql_server = "localhost"
+        $mysql_user = "root"
+        $mysql_password = "eah+S`;<6Pj=5-m["
 
-    $cn = New-Object -TypeName MySql.Data.MySqlClient.MySqlConnection
-    $cn.ConnectionString = "SERVER=$mysql_server;UID=$mysql_user;PWD=$mysql_password"
-    $cn.Open()
-    write-host "Running backup script against db1"
-    # Run Update Script MySQL
-    $cm = New-Object -TypeName MySql.Data.MySqlClient.MySqlCommand
-    $sql = Get-Content "createTestData.sql"
-    $cm.Connection = $cn
-    $cm.CommandText = $sql
-    $cm.ExecuteReader()
-    write-host "Closing Connection"
-    $cn.Close()
+        mysql
+    } catch {
+        Write-Error "Database setup failed, is mysql installed?"
+    }
 }
 else
 {
     Write-Warning "No Database creation script found. Database will not be updated"
 }
 
+Write-Host "Copying Files"
+
 Remove-Item "$PSScriptRoot\..\OST\*" -Recurse -Force
 
 Copy-Item "$PSScriptRoot\*" -Destination "$PSScriptRoot\..\OST\" -Recurse -Exclude @(".*", "deployScript.ps1", "deployScript.bat")
+
+Write-Host "Deployed to $PSScriptRoot\..\OST\"
