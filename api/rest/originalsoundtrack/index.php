3e3a4dabd0b770e918aabdec1d40e22ab7e3f227
tauchner <7229@htl.rennweg.at>
2020-12-09 00:36:11 +0100

    - Added OST API

---
+++ api/rest/originalsoundtrack/index.php
@@ -0,0 +1,46 @@
+<?php
+
+spl_autoload_register(function ($class) {
+  include "../../../" . $class . '.class.php';
+});
+
+use LiveSchach\OriginalSoundTrack;
+
+$request = $_GET;
+
+$servername = "localhost";
+$username = "OstServiceUser";
+$password = "automationIsKing01";
+
+$conn = new PDO("mysql:host=$servername;dbname=OstCoverService", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$databaseRequest = $conn->prepare("select * from OriginalSoundTracks");
+if (array_key_exists("id", $request)) {
+  $id = explode(",", $request["id"]);
+  $in = str_repeat('?,', count($id) - 1) . '?';
+  $databaseRequest = $conn->prepare("select * from OriginalSoundTracks where id in($in)");
+  $databaseRequest->execute($id);
+} else {
+  $databaseRequest->execute();
+}
+
+$result = $databaseRequest->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($result)) {
+  $result = ["error" => "ID not found"];
+} else {
+  $resultCopy = $result;
+  $result = array();
+  foreach ($resultCopy as $item) {
+    $song = new OriginalSoundTrack($item["id"], $item["name"], $item["videoGame"], $item["releaseYear"], array());
+    $result[$item["id"]] = $song;
+  }
+}
+
+header('Content-Type: application/json');
+
+$result = json_encode($result);
+
+echo $result;
